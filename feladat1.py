a7000518f488691c46d511b9b2a445ee94778786
csuhaykrisztian <csuhay.krisztian@gmail.com>
2025-12-14 13:03:38 +0100

    feladat1.py

--- feladat1.py
+++ feladat1.py
@@ -1,5 +1,9 @@
 print("Ez a program kiírja a páros számokat 1 és 10 között.")
 
-for szam in range(1, 11):
+szam = 1
+
+while szam <= 10:
     if szam % 2 == 0:
         print(szam)
+    szam += 1
+
