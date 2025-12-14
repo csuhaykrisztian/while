9016d41eb0c6b7bb1880aafe99c8ffa97308ba04
csuhaykrisztian <csuhay.krisztian@gmail.com>
2025-12-14 13:04:21 +0100

    feladat3.py

--- feladat3.py
+++ feladat3.py
@@ -1,5 +1,8 @@
 print("Ez a program csökkenő sorrendben kiírja a páratlan számokat 1 és 10 között.")
 
-for szam in range(10, 0, -1):
+szam = 10
+
+while szam >= 1:
     if szam % 2 != 0:
         print(szam)
+    szam -= 1
